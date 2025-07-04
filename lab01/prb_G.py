5ce745cca61863a12d55866af05eb0e07c619b98
Abir <144364412+Hafiz-110@users.noreply.github.com>
2025-07-04 22:35:05 +0600

    reduced condition and printed the values converting to integer

--- lab01/prb_G.py
+++ lab01/prb_G.py
@@ -1,25 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-def again_sort(length):
-    idd = input().split(); mark = input().split()
-    swaps = 0; 
-    for i in range(length-1):
-        max_i = i; flag = True
-        for j in range(i+1, length):
+def again_sort(ln):
+    idd = input().split(' '); mark = input().split(' ')
+    swaps = 0
+    for i in range(ln-1):
+        flag = False; max_i = i
+        for j in range(i+1,ln):
             if int(mark[max_i])<int(mark[j]):
-                max_i = j; flag = False
-            elif int(mark[max_i])==int(mark[j]):
-                if int(idd[max_i])>int(idd[j]):
-                    max_i = j; flag = False
-        # swap
+                max_i = j; flag = True
+            elif (int(mark[max_i])==int(mark[j])) and (int(idd[max_i])>int(idd[j])):
+                max_i = j; flag = True
+        #swap
         mark[i], mark[max_i] = mark[max_i], mark[i]
         idd[i], idd[max_i] = idd[max_i], idd[i]
-        
-        if flag == False: swaps += 1; 
-            
+        if flag: swaps += 1
     print(f'Minimum swaps: {swaps}')
-    for i in range(length):
-        print(f'ID: {idd[i]} Mark: {mark[i]}')
+    for i in range(ln):
+        print(f'ID: {int(idd[i])} Mark: {int(mark[i])}')
 
 again_sort(int(input()))
