f0eb5031fe609ef9337ea690e17f8a0247698a77
Abir <144364412+Hafiz-110@users.noreply.github.com>
2025-07-04 21:54:56 +0600

    used fast input method. watch out for \n!!!

--- lab01/prb_A.py
+++ lab01/prb_A.py
@@ -1,10 +1,13 @@
+import sys
+input = sys.stdin.readline
+
 def even_odd(i):
     for j in range(i):
-        inp = int(input())
-        if inp%2 == 0:
-            print(f"{inp} is an Even number.")
+        inp = input()
+        if int(inp)%2 == 0:
+            print(f"{int(inp)} is an Even number.")
         else:
-            print(f"{inp} is an Odd number.")
+            print(f"{int(inp)} is an Odd number.")
 
 num_inp = int(input())
 even_odd(num_inp)
