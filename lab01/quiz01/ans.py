5bc6235ea27d98790bfcb15dbd0512801bf32707
Abir <144364412+Hafiz-110@users.noreply.github.com>
2025-07-05 12:48:44 +0600

    Create ans.py

---
+++ lab01/quiz01/ans.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+def fast_sum(test):
+    for i in range(test):
+        n = int(input())
+        sum = n*(n+1)/2
+        print(round(sum, 6))
+
+
+
+fast_sum(int(input()))
+
+
+exit(0)
