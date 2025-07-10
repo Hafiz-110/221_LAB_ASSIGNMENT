72687eed05c58c7f3acf3b5b1fd18c93672f95ca
Abir <144364412+Hafiz-110@users.noreply.github.com>
2025-07-10 22:09:51 +0600

    initial commit

---
+++ lab02/prb_A.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+def two_sum_trouble(f_line, arr):
+    length, target = int(f_line[0]), int(f_line[1])
+    i, j = 0, len(arr)-1
+
+    while i<j:
+        if int(arr[i])+int(arr[j]) == target:
+            return f'{i+1} {j+1}'
+        else:
+            if int(arr[i])+int(arr[j]) > target:
+                j -= 1
+            else:
+                i += 1
+    return '-1'
+
+
+first_line = input().split()
+arr = input().split()
+print(two_sum_trouble(first_line, arr))
