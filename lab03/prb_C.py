aba5fbc371d9eae56ba2cc50e7a457084733cd83
Abir <144364412+Hafiz-110@users.noreply.github.com>
2025-07-19 11:41:40 +0600

    binary exponentiation

---
+++ lab03/prb_C.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+def fast_power(a, b):
+    res = 1
+    a = a % 107
+    while b > 0:
+        if b%2 == 1:
+            res = (res*a) % 107
+        a = (a*a) % 107
+        b //= 2
+    return res
+
+a, b = list(map(int, input().split()))
+print(fast_power(a, b))
