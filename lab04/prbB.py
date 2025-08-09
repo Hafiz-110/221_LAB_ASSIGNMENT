d980046306408acce00cefa5abe6a1f0ca3730e1
Abir <144364412+Hafiz-110@users.noreply.github.com>
2025-08-09 07:38:00 +0600

    removed condition in first loop

--- lab04/prbB.py
+++ lab04/prbB.py
@@ -2,22 +2,16 @@ import sys
 input = sys.stdin.readline
 
 def adjacency_list(n, m):
-    dic = {i+1:[()] for i in range(n)}    # python way to create dic
+    dic = {i+1:[()] for i in range(n)}
     n1 = list(map(int, input().split()))
     n2 = list(map(int, input().split()))
     w = list(map(int, input().split()))
-
     for i in range(m):
-        if n1[i] not in dic:
-            dic[n1[i]] = [(n2[i], w[i])]
-        else: dic[n1[i]] +=  [(n2[i], w[i])]
-
+        dic[n1[i]].append((n2[i], w[i]))
+    
     for k, v in dic.items():
-        if v==[()]:
-            print(f'{k}:')
-        else:
-            print(f'{k}:', *v[1:])
-
+        if v==[()]: print(f'{k}:')
+        else: print(f'{k}:', *v[1:])
 
 n, m = list(map(int, input().split()))
 adjacency_list(n, m)
