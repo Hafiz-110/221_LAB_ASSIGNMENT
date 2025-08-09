c5b8af51821f93786c19418d05ae5e8f626eb0ee
Abir <144364412+Hafiz-110@users.noreply.github.com>
2025-08-09 08:08:59 +0600

    Update prbD.py

--- lab04/prbD.py
+++ lab04/prbD.py
@@ -1,20 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-def seven_bridges(n, m):    #eulerian path
+def seven_bridges(n, m):
     n1 = list(map(int, input().split()))
     n2 = list(map(int, input().split()))
-    d = [0]*(n+1)
+    d = [0 for _ in range(n)]
     for i in range(m):
-        d[n1[i]] += 1; d[n2[i]] += 1
-
+        d[n1[i]-1] += 1
+        d[n2[i]-1] += 1
     count = 0
     for i in d:
-        if i%2!=0: count +=1    # count = sum(1 for i in d if i%2!=0)
-
-    if count == 0 or count ==2: return f'YES'
-    else: return f'NO'
-
+        if i%2!=0: count += 1
+    if count==0 or count==2: print('YES')
+    else: print('NO')
 
 n, m = list(map(int, input().split()))
-print(seven_bridges(n, m))
+seven_bridges(n, m)
